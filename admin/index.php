2ff578494faf853c99dafcaddeb80b6663f08e87
Rafat <ahsanhabibrafat11@gmail.com>
2025-08-13 00:27:48 +0600

    Dashboard Fixed CHECKPOINT

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,8 @@
     .card {
         border: none;
         transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+        min-height: 180px;
+        padding: 20px;
     }
 
     .card:hover {
@@ -12,9 +14,19 @@
         box-shadow: 0 4px 10px rgba(0, 0, 0, 0.15);
     }
 
+    .card-body {
+        padding: 2rem !important;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+
     .card-title {
-        font-size: 1.2rem;
+        font-size: 1.5rem;
         font-weight: bold;
+        margin-bottom: 1.5rem;
     }
 
     .fw-bold {
